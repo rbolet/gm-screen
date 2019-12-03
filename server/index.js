eb9370e83aea67fd69757fcfbb32dd0b3a3799aa
re-bo <dev@404-NOTFOUND.localdomain>
2019-12-02 19:06:35 -0800

    starting on backend

--- server/index.js
+++ server/index.js
@@ -1,7 +1,32 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const http = require('http').createServer(app);
+// const http = require('http').createServer(app);
 const multer = require('multer');
 const uuid = require('uuid/v4');
 const bodyParser = require('body-parser');
+
+// make public folder files available, such as index.html
+const staticPath = path.join(__dirname, 'public');
+const staticMiddleware = express.static(staticPath);
+app.use(staticMiddleware);
+app.use(bodyParser.json());
+
+// upload images
+var storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, '/public/images'));
+  },
+  filename: (req, file, cb) => {
+    var parsed = path.parse(file.originalname);
+    cb(null, `${uuid()}.${parsed.ext}`);
+  }
+});
+var upload = multer({
+  storage: storage
+});
+
+app.post('/upload', upload.single('image-upload'), (req, res) => {
+  // console.log(req.file.filename);
+  res.status(200).json({ filename: req.file.filename });
+});
