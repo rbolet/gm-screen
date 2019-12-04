d03af67373a74c5f02eb0d39d5d5b2e55097bfbb
Rapha Bolet <rapha.bolet@gmail.com>
2019-12-04 09:46:08 -0800

    Added a session-view component, other views will be their own component

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,42 +1,19 @@
 const React = require('react');
-const SessionImageList = require('./session-image-list');
-const Thumbnail = require('./thumbnail');
+const SessionView = require('./session-view');
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      imagesArray: [],
-      thumbnailImage: null
+      view: 'session'
     };
-
-    this.changeThumbnail = this.changeThumbnail.bind(this);
-  }
-
-  changeThumbnail(image) {
-    this.setState({ thumbnailImage: image });
-  }
-
-  componentDidMount() {
-    fetch('/api/imagelist', { method: 'GET' })
-      .then(res => res.json())
-      .then(imagesArray => {
-        this.setState({ imagesArray });
-      })
-      .catch(error => {
-        alert(`Error in GET return: ${error}`);
-      });
   }
 
   render() {
-
     return (
       <div className="app-container container-fluid vh-100 px-0">
         <header className="app-header"></header>
-        <div className="app-body row">
-          <SessionImageList images={this.state.imagesArray} changeThumbnail={this.changeThumbnail}/>
-          <Thumbnail thumbnailImage={this.state.thumbnailImage}/>
-        </div>
+        <SessionView/>
       </div>
     );
   }
