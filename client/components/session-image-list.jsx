ac42f860f0ff1de629580a33b53a289d8ef542c1
Rapha Bolet <rapha.bolet@gmail.com>
2019-12-03 19:19:05 -0800

    Properly adding 'selected' class to row on click

--- client/components/session-image-list.jsx
+++ client/components/session-image-list.jsx
@@ -1,22 +1,40 @@
 const React = require('react');
 
-function SessionImageList(props) {
+class SessionImageList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedRowKey: null
+    };
 
-  return (
-    <div className="col-4 h-100">
-      <div className="all-images-header text-white text-center">My Session</div>
-      <div className="all-images-body bg-light h-75">
-        <table className="table">
-          <thead>
-            <tr>
-              <th>File Name</th>
-            </tr>
-          </thead>
-          <SessionImage images={props.images}/>
-        </table>
+    this.onRowClick = this.onRowClick.bind(this);
+  }
+
+  onRowClick(row) {
+    this.setState({ selectedRowKey: row });
+  }
+
+  render() {
+    return (
+      <div className="col-4 h-100">
+        <div className="all-images-header text-white text-center">My Session</div>
+        <div className="all-images-body bg-light h-75">
+          <table className="table">
+            <thead>
+              <tr>
+                <th>File Name</th>
+              </tr>
+            </thead>
+            <SessionImage
+              images={this.props.images}
+              selectedRow={this.state.selectedRowKey}
+              onClick={this.onRowClick}
+            />
+          </table>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 module.exports = SessionImageList;
@@ -25,7 +43,10 @@ function SessionImage(props) {
 
   const elementRows = props.images.map(image => {
     return (
-      <tr key={image.imageId}>
+      <tr
+        key={image.imageId}
+        className={props.selectedRow === image.imageId ? 'selected' : ''}
+        onClick={props.onClick.bind(this, image.imageId)}>
         <td>{image.filename}</td>
       </tr>
     );
