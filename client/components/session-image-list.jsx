bb919b4f745fe1fa36360e336bf72897aabc5f43
Rapha Bolet <rapha.bolet@gmail.com>
2019-12-04 15:54:06 -0800

    working on sliding upload form element

--- client/components/session-image-list.jsx
+++ client/components/session-image-list.jsx
@@ -20,7 +20,21 @@ class SessionImageList extends React.Component {
       <div className="col h-100 d-inline-block">
         <div className="all-images-header text-white text-center mt-2">My Session</div>
         <div className="all-images-body rounded bg-light h-75">
-          {/* <div className="rounded d-flex w-100 color-quartz text-white p-2">File Name</div> */}
+          <div className="transparent h-50"></div>
+          <div className="upload-slidedown color-quartz p-0">
+            <div className="d-flex justify-content-center">
+              <button className="btn" type="submit">Upload</button>
+            </div>
+            <form className="invisible" onSubmit={this.handleSubmit}>
+              <div className="form-group">
+                <input className="form-control" type="text"/>
+              </div>
+              <div className="custom-file">
+                <input type="file" className="custom-file-input" id="customFile"/>
+                <label className="custom-file-label" htmlFor="customFile">Choose file</label>
+              </div>
+            </form>
+          </div>
           <table className="table images-table">
             <SessionImage
               images={this.props.images}
