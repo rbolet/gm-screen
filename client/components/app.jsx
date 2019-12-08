31ca2cc7a4b15f5720d505e432c9d5990637bf74
Rapha Bolet <rapha.bolet@gmail.com>
2019-12-07 21:22:51 -0800

    Now begins on a main menu screen, clicking 'Game Master' goes to session screen.

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -8,21 +8,28 @@ class App extends React.Component {
     this.state = {
       view: 'menu'
     };
+
+    this.goToSessionView = this.goToSessionView.bind(this);
+  }
+
+  goToSessionView() {
+    this.setState({ view: 'session' });
   }
 
   render() {
+    let currentView;
+    switch (this.state.view) {
+      case 'menu':
+        currentView = <MenuView goToSessionView={this.goToSessionView}/>;
+        break;
+      case 'session':
+        currentView = <SessionView/>;
+        break;
+    }
     return (
       <div className="app-container container-fluid vh-100 px-0">
         <header className="app-header color-quartz"></header>
-        {() => {
-          switch (this.state.view) {
-            case 'session':
-              return <SessionView/>;
-            case 'menu' :
-              return <MenuView/>;
-          }
-        }
-        }
+        {currentView}
       </div>
     );
   }
--- client/components/menu-view.jsx
+++ client/components/menu-view.jsx
@@ -1,16 +1,15 @@
 const React = require('react');
 
-class MenuView extends React.Component {
-  render() {
-    return (
-      <div className="view-body d-flex justify-content-center align-items-center">
-        <div className="menu-container h-75 w-25 bg-dark rounded d-flex flex-column justify-content-center p-4">
-          <button className="btn btn-secondary mb-4">Game Master</button>
-          <button className="btn btn-secondary mb-4">Player</button>
-        </div>
+function MenuView(props) {
+
+  return (
+    <div className="view-body d-flex justify-content-center align-items-center">
+      <div className="menu-container h-75 w-25 bg-dark rounded d-flex flex-column justify-content-center p-4">
+        <button onClick={props.goToSessionView} className="btn btn-secondary mb-4">Game Master</button>
+        <button className="btn btn-secondary mb-4">Player</button>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 module.exports = MenuView;
