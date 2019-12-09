e4806ccc6b2928c914189c34bb4f3ed126c08794
Rapha Bolet <rapha.bolet@gmail.com>
2019-12-09 13:52:24 -0800

    put what was formerly a modal into menu component, still working on it

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -8,7 +8,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      view: 'player'
+      view: 'menu'
     };
 
     this.goToSessionView = this.goToSessionView.bind(this);
--- client/components/menu-view.jsx
+++ client/components/menu-view.jsx
@@ -1,14 +1,59 @@
 const React = require('react');
 
-function MenuView(props) {
+class MenuView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentMenu: 'playerChooseSession'
+    };
+  }
+
+  render() {
+    let currentMenu;
+
+    switch (this.state.currentMenu) {
+      case 'chooseRole':
+        currentMenu = <UserChooseRole goToSessionView={this.props.goToSessionView}/>;
+        break;
+      case 'playerChooseSession':
+        currentMenu = <PlayerChooseSession/>;
+        break;
+    }
+
+    return (
+      <div className="view-body d-flex justify-content-center align-items-center">
+        {currentMenu};
+      </div>
+    );
+  }
+}
+
+function UserChooseRole(props) {
 
   return (
-    <div className="view-body d-flex justify-content-center align-items-center">
-      <div className="menu-container h-75 w-25 bg-dark rounded d-flex flex-column justify-content-center p-4">
-        <button onClick={props.goToSessionView} className="btn btn-secondary mb-4">Game Master</button>
-        <button className="btn btn-secondary mb-4">Player</button>
+    <div className="menu-container h-75 w-25 bg-dark rounded d-flex flex-column justify-content-center p-4">
+      <h5 className="text-light text-center">Choose Your Role</h5>
+      <button onClick={props.goToSessionView} className="btn btn-secondary mb-4">Game Master</button>
+      <button className="btn btn-secondary mb-4">Player</button>
+    </div>
+  );
+}
+
+function PlayerChooseSession(props) {
+
+  return (
+    <div className="menu-container h-75 w-25 bg-dark rounded d-flex flex-column justify-content-center p-4">
+      <h5 className="text-light text-center">Join Session</h5>
+      <div className="bg-light text-dark h-75">
+        <p>Session</p>
+        <p>Session</p>
+        <p>Session</p>
+      </div>
+      <div className="modal-footer">
+        <button type="button" className="btn btn-secondary">Join Session</button>
       </div>
     </div>
+
   );
 }
 
