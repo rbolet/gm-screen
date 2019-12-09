726fab0157d7240ce9cb6bc227b80da14bd7e20f
Rapha Bolet <rapha.bolet@gmail.com>
2019-12-09 15:01:59 -0800

    Can click through menus to both player and GM views

--- client/components/menu-view.jsx
+++ client/components/menu-view.jsx
@@ -7,15 +7,13 @@ class MenuView extends React.Component {
       currentMenu: 'chooseRole'
     };
 
-    this.goToPlayerSessionView = this.gotoPlayerSessionView.bind(this);
+    this.goToPlayerSessionView = this.goToPlayerSessionView.bind(this);
   }
 
   goToPlayerSessionView() {
     fetch('/api/allsessions', { method: 'GET' })
-      .then(res => {
-        console.log(res);
-        return res;
-      });
+      .then(res => res.json())
+      .then(jsonRes => jsonRes);
     this.setState({ currentMenu: 'playerChooseSession' });
   }
 
@@ -24,7 +22,9 @@ class MenuView extends React.Component {
 
     switch (this.state.currentMenu) {
       case 'chooseRole':
-        currentMenu = <UserChooseRole goToSessionView={this.props.goToSessionView}/>;
+        currentMenu = <UserChooseRole
+          goToPlayerSessionView={this.goToPlayerSessionView}
+          goToSessionView={this.props.goToSessionView}/>;
         break;
       case 'playerChooseSession':
         currentMenu = <PlayerChooseSession playerJoinSession={this.props.playerJoinSession}/>;
@@ -45,7 +45,7 @@ function UserChooseRole(props) {
     <div className="menu-container h-75 w-25 bg-dark rounded d-flex flex-column justify-content-center p-4">
       <h5 className="text-light text-center">Choose Your Role</h5>
       <button onClick={props.goToSessionView} className="btn btn-secondary mb-4">Game Master</button>
-      <button onClick={this.goToPlayerSessionView} className="btn btn-secondary mb-4">Player</button>
+      <button onClick={props.goToPlayerSessionView} className="btn btn-secondary mb-4">Player</button>
     </div>
   );
 }
@@ -55,7 +55,7 @@ function PlayerChooseSession(props) {
   return (
     <div className="menu-container h-75 w-25 bg-dark rounded d-flex flex-column justify-content-center p-4">
       <h5 className="text-light text-center">Join Session</h5>
-      <div className="bg-light text-dark h-75">
+      <div className="bg-light text-dark h-75 p-2">
         <p onClick={props.playerJoinSession}>Landing on Nar Shadda</p>
       </div>
       <div className="modal-footer">
