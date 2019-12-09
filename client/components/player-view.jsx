14592d858459e6544fc7930729aa87c0e587d669
Rapha Bolet <rapha.bolet@gmail.com>
2019-12-09 12:51:16 -0800

    hard-coded Player View working properly

--- client/components/player-view.jsx
+++ client/components/player-view.jsx
@@ -4,7 +4,7 @@ class PlayerView extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      environment: '3d0e382a-7376-4802-9984-f936f4d04fbf..jpg'
+      environmentImage: '3d0e382a-7376-4802-9984-f936f4d04fbf..jpg'
     };
   }
 
@@ -15,7 +15,7 @@ class PlayerView extends React.Component {
         <div className="row h-100 w-100">
           <div className="col-12 h-100 border border-warning">
             <div className="h-100 p-2 border border-success">
-              <div className="environment-image h-100 w-100" style="background-image: url(../server/public/images/3d0e382a-7376-4802-9984-f936f4d04fbf..jpg);"></div>
+              <div className="environment-image h-100 w-100" style={{ backgroundImage: `url(./images/${this.state.environmentImage})` }}></div>
             </div>
           </div>
         </div>
