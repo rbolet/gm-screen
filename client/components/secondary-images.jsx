51b5fbe33e24d56e580c37ec3e7c1bc0a6863de3
Rapha Bolet <rapha.bolet@gmail.com>
2019-12-11 13:37:02 -0800

    all secondary images clearing properly for GM view

--- client/components/secondary-images.jsx
+++ client/components/secondary-images.jsx
@@ -1,9 +1,18 @@
 const React = require('react');
 
 function SecondaryImages(props) {
+  let closeButton = null;
+  if (props.gmClick) {
+    closeButton = (
+      <div className="close m-1">
+        <i className="fa fa-times-circle" onClick={props.gmClick}></i>
+      </div>
+    );
+  }
   return (
     <div className="secondary-images-container d-flex justify-content-center" id={props.id ? props.id : ''}>
       <SecondaryImage secondaryImagesArray={props.secondaryImagesArray}/>
+      {closeButton}
     </div>
   );
 }
