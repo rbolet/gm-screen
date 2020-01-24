a389a4ed888bade4436ade4eb19eec9f073e4e3b
Rapha Bolet <rapha.bolet@gmail.com>
2020-01-24 10:42:00 -0800

    new sql dump w/o images

---
+++ database/gm_screen2020_noimages.sql
@@ -0,0 +1,171 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jan 24, 2020 at 10:41 AM
+-- Server version: 5.7.27-0ubuntu0.18.04.1
+-- PHP Version: 7.2.19-0ubuntu0.18.04.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `gm_screen`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `images`
+--
+
+CREATE TABLE `images` (
+  `imageId` int(11) NOT NULL,
+  `fileName` varchar(200) CHARACTER SET latin1 NOT NULL,
+  `userGivenName` varchar(120) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `category` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sessionImages`
+--
+
+CREATE TABLE `sessionImages` (
+  `sessionImageId` int(11) NOT NULL,
+  `sessionId` int(11) NOT NULL,
+  `imageId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `sessionImages`
+--
+
+INSERT INTO `sessionImages` (`sessionImageId`, `sessionId`, `imageId`) VALUES
+(5, 1, 25),
+(6, 1, 26),
+(7, 1, 27),
+(8, 1, 28),
+(9, 1, 29),
+(10, 1, 30),
+(12, 1, 32),
+(13, 1, 33),
+(14, 1, 34),
+(15, 1, 35),
+(16, 1, 36),
+(17, 1, 37),
+(18, 1, 38),
+(19, 1, 39),
+(20, 1, 40),
+(21, 1, 41),
+(22, 1, 42),
+(23, 1, 43),
+(24, 1, 44),
+(25, 1, 45),
+(26, 1, 46),
+(27, 1, 47),
+(28, 1, 48),
+(29, 1, 49);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sessions`
+--
+
+CREATE TABLE `sessions` (
+  `sessionId` int(11) NOT NULL,
+  `sessionName` varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `sessionGM` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `sessions`
+--
+
+INSERT INTO `sessions` (`sessionId`, `sessionName`, `sessionGM`) VALUES
+(1, 'Sneak into Imp Base', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `userId` int(11) NOT NULL,
+  `userName` varchar(80) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`userId`, `userName`, `password`) VALUES
+(1, 'AdmiralAckbar', 'itsatrap'),
+(2, 'GoldOne', 'asplode');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `images`
+--
+ALTER TABLE `images`
+  ADD PRIMARY KEY (`imageId`);
+
+--
+-- Indexes for table `sessionImages`
+--
+ALTER TABLE `sessionImages`
+  ADD PRIMARY KEY (`sessionImageId`);
+
+--
+-- Indexes for table `sessions`
+--
+ALTER TABLE `sessions`
+  ADD PRIMARY KEY (`sessionId`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`userId`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `images`
+--
+ALTER TABLE `images`
+  MODIFY `imageId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
+--
+-- AUTO_INCREMENT for table `sessionImages`
+--
+ALTER TABLE `sessionImages`
+  MODIFY `sessionImageId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+--
+-- AUTO_INCREMENT for table `sessions`
+--
+ALTER TABLE `sessions`
+  MODIFY `sessionId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
