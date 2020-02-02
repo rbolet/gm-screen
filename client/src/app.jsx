8f4555d7c13c3836b6f83ce34fc0295430b3c376
Rapha Bolet <rapha.bolet@gmail.com>
2020-02-02 12:58:19 -0800

    component and app state skeleton in place

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,15 +1,50 @@
 import React from 'react';
 import Header from './components/header';
+import MenuView from './views/menu-view';
 
-class App extends React.Component{
-  render(){
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      view: 'menu',
+      message: '',
+      config: {
+        user: {
+          userId: null,
+          userName: null,
+          userRole: null,
+          socketId: null
+        },
+        gameSession: {
+          campaignId: null,
+          campaignName: null,
+          sessionId: null,
+          sessionName: null,
+          sessionAssets: [],
+          sessionState: {
+            sessionUsers: {
+              gm: null,
+              players: []
+            },
+            environmentImage: null,
+            activeSecondaries: []
+          }
+        }
+      }
+    };
+  }
+
+  render() {
+    const CurrentView = <MenuView config={this.state.config}/>;
     return (
-      <div className="app-header">
+      <div className="app h-100 w-100">
         <Header/>
+        <div className="app-body">
+          {CurrentView}
+        </div>
       </div>
-      <div className="app-body">
-
-      </div>
-    )
+    );
   }
 }
+export default App;
