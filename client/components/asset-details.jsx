2747a5916cfa7e0b07e186cbd3141e5b35a02331
Rapha Bolet <rapha.bolet@gmail.com>
2020-02-14 10:02:33 -0800

    added text to launch session button

--- client/components/asset-details.jsx
+++ client/components/asset-details.jsx
@@ -9,7 +9,10 @@ class AssetDetails extends React.Component {
             <img className="img-thumbnail mh-100" src={`./images/${this.props.assetImage.fileName}`}/>}
         </div>
         <div className=" h-25 detail-footer justify-content-center align-items-center row">
-          <button type="button" onClick={this.props.connectSocket} className="btn btn-lg btn-success justify-self-end w-25"><i className="fas fa-play"></i></button>
+          <button type="button" onClick={this.props.connectSocket} className="btn btn-lg btn-success justify-self-end w-25">
+            <i className="fas fa-play"/>
+            <p className="button-text m-0">Launch Session</p>
+          </button>
         </div>
       </div>
     );
