6e362d276fbf8b794bc8070c21cc2c4e94b29f2d
Rapha Bolet <rapha.bolet@gmail.com>
2020-03-06 14:58:28 -0800

    basic idea in place

--- client/components/text-chat.jsx
+++ client/components/text-chat.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Accordion from 'react-bootstrap/Accordion';
 import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
 
 export default function TextChatWindow() {
 
@@ -18,7 +20,9 @@ export default function TextChatWindow() {
       </Card>
       <Card>
         <Accordion.Toggle as={Card.Header} eventKey="0" className="chat-footer bg-dark">
-          Click me!
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="" />
+          </Form>
         </Accordion.Toggle>
       </Card>
     </Accordion>
